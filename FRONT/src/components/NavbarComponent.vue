0740e0602ba1e2b2e51a34bb8c58e7a271c2e978
LibertadLorda <libertadlorda@gmail.com>
2023-09-04 16:36:30 +0200

    add basic vue and test

---
+++ FRONT/src/components/NavbarComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+    <v-app-bar :elevation="6" color="primary">
+        <v-toolbar-title class="text-no-wrap" title="Application">Camping el Molino</v-toolbar-title>
+        <v-btn class="button" size="medium" color="surface-variant" variant="text"><i class="fas fa-campground fa-lg"></i>Fotos</v-btn>
+        <v-btn class="button" size="medium" color="surface-variant" variant="text"><i class="fas fa-leaf fa-lg"></i>Instalaciones</v-btn>
+        <v-btn class="button" size="medium" color="surface-variant" variant="text"><i class="fas fa-euro-sign fa-lg"></i>Tarifas</v-btn>
+        <v-btn class="button" size="medium" color="surface-variant" variant="text"><i class="fas fa-video fa-lg"></i>Webcam</v-btn>
+    </v-app-bar>
+
+    </div>
+</template>
+
+<style scoped>
+
+.button {
+  margin-right: 2rem;
+}
+
+
+</style>
---
+++ FRONT/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import FooterComponent from '@/components/FooterComponent.vue';
+import NavbarComponentVue from '@/components/NavbarComponent.vue';
+import HelloWorld from '@/components/HelloWorld.vue';
+
+</script>
+
+<template>
+  <NavbarComponentVue/>
+  <HelloWorld/>
+  <FooterComponent/>
+</template>
