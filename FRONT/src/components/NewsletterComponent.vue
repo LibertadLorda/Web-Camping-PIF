d390149ca929dc95619613e81591d1a651f937d0
LibertadLorda <libertadlorda@gmail.com>
2023-09-12 08:35:15 +0200

    add news component and bd

--- FRONT/src/components/NewsletterComponent.vue
+++ FRONT/src/components/NewsletterComponent.vue
@@ -1,205 +1,3 @@
-<!-- <script setup>
-  import { ref } from 'vue'
-  import NewsletterService from '../services/NewsletterService';
-
-  const { handleSubmit, handleReset } = useForm({
-    validationSchema: {
-      nameSurname (value) {
-        if (!value || value.length < 2) 
-        return 'Este campo debe tener al menos 2 caracteres';
-        return true;
-      },
-      phone (value) {
-        if (!value || (value.length <= 9 && !/[0-9-]+/.test(value))) 
-        return 'Este campo debe ser un número de teléfono válido';
-        return true;
-      },
-      email (value) {
-        if (!value || !/^[a-z.-]+@[a-z.-]+\.[a-z]+$/i.test(value)) 
-        return 'Este campo debe ser un correo electrónico válido';
-        return true;
-      },
-      language (value) {
-        if (!value) return 'Seleccione el idioma en que lo quiere recibir.'
-        return true;
-      },
-    },
-  })
-  const nameSurname = useField('nameSurname')
-  const phone = useField('phone')
-  const email = useField('email')
-  const language = useField('language')
-
-  const items = ref([
-    'Español',
-    'Inglés',
-  ])
-
-  const submit = handleSubmit(values => {
-    alert(JSON.stringify(values, null, 2))
-  })
-
-const dataNews = async () =>{
-    if (nameSurname.value && phone.value && email.value && language.value ){
-        const Data = {
-            nameSurname: nameSurname.value.value,
-            email: email.value.value,
-            phone: phone.value.value,
-            language: language.value.value,
-    };
-        try {
-            await NewsletterService.post(Data);
-            console.log(Data);
-        }
-        catch (error) {
-            console.log(error);
-    }
-    }
-  
-};
-</script>
-<template>
-    <form @submit.prevent="submitForm">
-      <v-text-field
-        v-model="nameSurname.value.value"
-        :counter="10"
-        :error-messages="nameSurname.errorMessage.value"
-        label="Nombre y apellidos"
-      ></v-text-field>
-  
-      <v-text-field
-        v-model="phone.value.value"
-        :counter="9"
-        :error-messages="phone.errorMessage.value"
-        label="Número de teléfono"
-      ></v-text-field>
-  
-      <v-text-field
-        v-model="email.value.value"
-        :error-messages="email.errorMessage.value"
-        label="E-mail"
-      ></v-text-field>
-  
-      <v-select
-        v-model="language.value.value"
-        :items="items"
-        :error-messages="language.errorMessage.value"
-        label="Idioma"
-      ></v-select>
-  
-      <v-btn
-        class="me-4"
-        type="submit"
-        @click="dataNews()"
-      >
-        Enviar
-      </v-btn>
-  
-      <v-btn @click="handleReset">
-        Borrar 
-      </v-btn>
-    </form>
-  </template> -->
-<!-- 
-  <template>
-    <form @submit.prevent="submitForm">
-      <v-text-field
-        v-model="formData.nameSurname"
-        label="Nombre y apellidos"
-      ></v-text-field>
-      <span v-if="nameError" class="error">{{ nameError }}</span>
-  
-      <v-text-field
-        v-model="formData.phone"
-        label="Número de teléfono"
-      ></v-text-field>
-      <span v-if="phoneError" class="error">{{ phoneError }}</span>
-  
-      <v-text-field
-        v-model="formData.email"
-        label="E-mail"
-      ></v-text-field>
-      <span v-if="emailError" class="error">{{ emailError }}</span>
-  
-      <v-select
-        v-model="formData.language"
-        :items="items"
-        label="Idioma"
-      ></v-select>
-      <span v-if="languageError" class="error">{{ languageError }}</span>
-
-  
-      <v-btn type="submit">Enviar</v-btn>
-      <v-btn @click="resetForm">Borrar</v-btn>
-    </form>
-  </template>
-  
-  <script setup>
-  import { ref } from 'vue'
-  import NewsletterService from '../services/NewsletterService';
-  
-  const formData = {
-    nameSurname: '',
-    phone: '',
-    email: '',
-    language: '',
-  };
-  
-  const nameSurnameError = ref('');
-  const phoneError = ref('');
-  const emailError = ref('');
-  const languageError = ref('');
-  
-  const items = ref(['Español', 'Inglés']);
-  
-  const validateForm = () => {
-    nameSurnameError.value = formData.nameSurname.length < 2 ? 'Este campo debe tener al menos 2 caracteres' : '';
-    phoneError.value = !/^\d{9}$/.test(formData.phone) ? 'Este campo debe ser un número de teléfono válido' : '';
-    emailError.value = !/^[a-z.-]+@[a-z.-]+\.[a-z]+$/i.test(formData.email) ? 'Este campo debe ser un correo electrónico válido' : '';
-    languageError.value = !formData.language ? 'Seleccione el idioma en que lo quiere recibir.' : '';
-  };
-  
-  const submitForm = async () => {
-    validateForm();
-  
-    if (!nameSurnameError.value && !phoneError.value && !emailError.value && !languageError.value) {
-        const Data = {
-        nameSurname: formData.nameSurname,
-        email: formData.email,
-        phone: formData.phone,
-        language: formData.language,
-    };
-    try {
-            await NewsletterService.post(Data);
-            console.log(Data);
-            resetForm();
-        }
-        catch (error) {
-            console.log(error);
-    }
-    }  
-}
-  
-  const resetForm = () => {
-    formData.nameSurname = '';
-    formData.phone = '';
-    formData.email = '';
-    formData.language = '';
-  
-    nameSurnameError.value = '';
-    phoneError.value = '';
-    emailError.value = '';
-    languageError.value = '';
-  };
-  </script>
-  
-  <style>
-  .error {
-    color: red;
-  }
-  </style>
-   -->
-
 <script setup>
 import { ref, computed } from 'vue'
 import NewsletterService from '../services/NewsletterService';
@@ -216,11 +14,15 @@ const items = ref([
 const form = ref("");
 const showConfirmation = ref(false);
 
+const closeForm= () => {
+      showConfirmation.value = false;
+};
+
 const formOk = computed(() =>{
     return (
-        !! nameSurname.value &&
-        phone.value.length === 9 &&
-        !! email.value &&
+        !! nameSurname.value >= 2 &&
+        (phone.value.length === 9 && /[0-9-]+/.test(value)) &&
+        !! email.value (/^[a-z.-]+@[a-z.-]+\.[a-z]+$/i.test(value)) &&
         (language.value === 'Español' || language.value === 'Inglés')
     );
 });
@@ -286,15 +88,28 @@ const clearForm = () => {
         v-model="language"
         :items="items"
         :rules="[v => !!v || 'El idioma es necesario.']"
-        label="Seleccione el idioma en el que quiere recibir el boletín."
+        label="Seleccione el idioma del boletín."
         required
       ></v-select>
-
+        <div class="btnForm">
       <v-btn ref="formBtn" class="mt-4" block :disabled="!formOk" @click="validateForm">Enviar</v-btn>
+    </div>
+    <div class="btnForm">
       <v-btn @click="clearForm">Borrar</v-btn>
-      <div v-if="showConfirmation" class="confirmMessage">
-        ¡El formulario se ha enviado correctamente!
-      </div>
+    </div>
+      <v-dialog v-model="showConfirmation" max-width="400">
+      <v-card>
+        <div class="py-4 text-center">
+          <v-icon class="mb-6" color="primary" icon="mdi-check-circle-outline" size="120"></v-icon>
+        </div>
+        <v-card-text class="py-3 text-center">
+          ¡Pronto recibirás noticias nuestras!
+        </v-card-text>
+        <v-card-actions class="d-flex justify-center">
+          <v-btn color="primary" @click="closeForm">Cerrar</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
     </form>
   </template>
 
@@ -303,7 +118,7 @@ const clearForm = () => {
 .mt-4 {
   text-decoration: none;
   transition: 0.3s;
-  background-color: rgba(54, 157, 178, 1);
+  background-color: green;
   color: white;
 }
 
@@ -314,7 +129,24 @@ const clearForm = () => {
 }
 
 .confirmMessage {
-  color: green;
-  margin-top: 10px;
+    color: green;
+    margin-top: 10px;
 }
+
+form{
+    width: 50vh;
+    margin: 0 auto;
+    padding: 20px;
+    background-color: #f0f0f0;
+    border-radius: 10px;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+}
+
+.btnForm{
+    text-align: center;
+    display: inline-block;
+    margin: 0 30px;
+    width: 30%;
+}
+
 </style>
